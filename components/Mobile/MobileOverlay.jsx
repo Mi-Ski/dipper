3b92e7b742b5670608f3f6962c0a655b4952f015
Mi-Ski <skibamichal@interia.pl>
2023-02-12 19:38:32 +0100

    mobile overlay fix

--- components/Mobile/MobileOverlay.jsx
+++ components/Mobile/MobileOverlay.jsx
@@ -6,11 +6,11 @@ const MobileOverlay = () => {
 	const user = useUser();
 
 	return (
-		<div className='block md:hidden fixed flex flex-col h-full justify-between z-[10]'>
-			<div className='w-screen p-7'>
+		<div className='block pointer-events-none md:hidden fixed flex flex-col h-full justify-between z-[10]'>
+			<div className='w-screen pointer-events-auto p-7'>
 				Picture
 			</div>
-			<div className='bg-contrast-posts w-screen p-7 flex justify-between'>
+			<div className='bg-contrast-posts pointer-events-auto w-screen p-7 flex justify-between'>
 				<div>logout</div>
 				<div>page 2</div>
 				<div>search</div>
