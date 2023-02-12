7ee9f32bceaa5c2d194562cee4a4543c6e68fd6a
Mi-Ski <skibamichal@interia.pl>
2023-02-12 23:21:26 +0100

    fix ms account profile pic

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
 	images: {
-    domains: ["random.imagecdn.app", "s.gravatar.com", "lh3.googleusercontent.com", "i.pravatar.cc"],
+    domains: ["random.imagecdn.app", "s.gravatar.com", "lh3.googleusercontent.com", "i.pravatar.cc", "apis.live.net"],
   },
 }
 
