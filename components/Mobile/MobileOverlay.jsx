6bef4adf22db301d5fdf1f58904b46162bf733ac
Mi-Ski <skibamichal@interia.pl>
2023-02-18 03:01:21 +0100

    onScroll conditional blob

--- components/Mobile/MobileOverlay.jsx
+++ components/Mobile/MobileOverlay.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MobileOverlayTop from "./MobileOverlayTop";
 import MobileOverlayBottom from "./MobileOverlayBottom";
 
 const MobileOverlay = () => {
+  const [topBlobShown, setTopBlobShown] = useState(false);
+	const [scrollPosition, setScrollPosition] = useState(0);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const position = window.pageYOffset;
+			setScrollPosition(position);
+			if (position > 200) {
+				setTopBlobShown(true);
+			} else {
+				setTopBlobShown(false);
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll, { passive: true });
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, [scrollPosition]);
+
+
 
   return (
     <div className="block pointer-events-none md:hidden fixed flex flex-col h-full justify-between z-[10]">
-			<MobileOverlayTop />
-			<MobileOverlayBottom />
+      {topBlobShown ? <MobileOverlayTop /> : <div></div>}
+      <MobileOverlayBottom />
     </div>
   );
 };
