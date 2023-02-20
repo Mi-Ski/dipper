8ab3a79fd3974defbddcfd9723b9e4d3457f3841
Mi-Ski <skibamichal@interia.pl>
2023-02-21 00:23:18 +0100

    addcomment

--- pages/api/tweets/addComment.js
+++ pages/api/tweets/addComment.js
@@ -1,4 +1,7 @@
-import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import {
+  getAccessToken,
+  withApiAuthRequired,
+} from "@auth0/nextjs-auth0";
 
 export default withApiAuthRequired(async function handler(req, res) {
   const { accessToken } = await getAccessToken(req, res);
@@ -18,27 +21,20 @@ export default withApiAuthRequired(async function handler(req, res) {
   const baseUrl = `${process.env.MONGODB_DATA_API_URL}/action`;
 
   try {
-    switch (req.method) {
-      case "PUT":
-        const updateData = await fetch(`${baseUrl}/updateOne`, {
-          ...fetchOptions,
-          body: JSON.stringify({
-            ...fetchBody,
-            filter: { _id: { $oid: req.body.postId } },
-            update: {
-              $push: {
-                comments: req.body.body,
-              },
-            },
-          }),
-        });
-        const updateDataJson = await updateData.json();
-        res.status(200).json(updateDataJson);
-        break;
-      default:
-        res.status(405).end();
-        break;
-    }
+    const updateData = await fetch(`${baseUrl}/updateOne`, {
+      ...fetchOptions,
+      body: JSON.stringify({
+        ...fetchBody,
+        filter: { _id: { $oid: req.body.postId } },
+        update: {
+          $push: {
+            comments: req.body.body,
+          },
+        },
+      }),
+    });
+    const updateDataJson = await updateData.json();
+    res.status(200).json(updateDataJson);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error });
