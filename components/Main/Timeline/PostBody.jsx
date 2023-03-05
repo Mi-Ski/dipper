ab11bcb2476dbc90fd9285c008a9ccde65141524
Mi-Ski <skibamichal@interia.pl>
2023-03-05 19:50:14 +0100

    hashtag functionality

--- components/Main/Timeline/PostBody.jsx
+++ components/Main/Timeline/PostBody.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import Image from "next/image";
 
 const PostBody = ({ user, body, postedAt }) => {
+  // wrap each word starting with # in a link redirecting to twitter search
+  const formattedBody = body.replace(
+    /#[a-zA-Z0-9]+/g,
+    (match) =>
+      `<a style="color: #4444ff; font-weight: 800;" href="https://twitter.com/search?q=%23${match.slice(
+        1,
+        match.length
+      )}">${match}</a>`
+  );
+
   return (
     <div className="flex border md:border px-4  md:px-10 pt-7 border-r-transparent border-l-transparent border-t-transparent  border-b-transparent border-solid">
       <div className="min-w-max rounded-full overflow-hidden md:drop-shadow-[0px_10px_10px_#000]">
@@ -26,7 +36,10 @@ const PostBody = ({ user, body, postedAt }) => {
             {new Date(+postedAt).toLocaleString()}
           </p>
         </div>
-        <div className="mt-7 mb-5 text-lg leading-8 break-all ">{body}</div>
+        <div
+          className="mt-7 mb-5 text-lg rich-text leading-8 break-all"
+          dangerouslySetInnerHTML={{ __html: formattedBody }}
+        ></div>
       </div>
     </div>
   );
